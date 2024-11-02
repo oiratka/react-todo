726cece068f9e54c590a5cc2ca6880d5da414057
Marina Mukobenova <167211285+oiratka@users.noreply.github.com>
2024-11-02 11:45:47 -0400

    TodoList.jsx added

---
+++ src/TodoList.jsx
@@ -0,0 +1,8 @@
+/* Create a new functional React component (see below)
+ Declare a function named TodoList
+ Export TodoList function as default module
+ Add a multi-line return statement to your TodoList function (this is where we will insert JSX)
+hint: use parenthesis for multi-line */
+
+
+
