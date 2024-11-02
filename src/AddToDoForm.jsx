08cdcf2a26c6ba123a0ac291ab371c11e84dbcb7
Marina Mukobenova <167211285+oiratka@users.noreply.github.com>
2024-11-02 13:00:28 -0400

    AddToDoForm and TodoList added

---
+++ src/AddToDoForm.jsx
@@ -0,0 +1,13 @@
+const AddToDoForm = () =>{
+    return <form action="">
+        <label htmlFor="todoTitle">Title</label>
+        <input type="text" id="todoTitle"/>
+        <button type="submit">Add</button>
+    </form>
+}
+
+
+
+
+
+export default AddToDoForm;
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -4,5 +4,31 @@
  Add a multi-line return statement to your TodoList function (this is where we will insert JSX)
 hint: use parenthesis for multi-line */
 
+const todoList = [
+    {
+      id: 1,
+      title: "set up React",
+      dueDate: "11/05/2024",
+    },
+    {
+      id: 2,
+      title: "push to Github",
+      dueDate: "11/05/2024",
+    },
+    {
+    id: 3,
+    title: "complete assignment",
+    dueDate: "11/05/2024",
+  },
+  ];
+  
 
+const TodoList = () => {
+    return <ul>
+{todoList.map ((todo) => (
+  <li key = {todo.id}>{todo.title} by: {todo.dueDate}</li>
+))}
+</ul>
+}
 
+export default TodoList;
