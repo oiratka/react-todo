1bccdad8c6f61f3fd9d9945250470b96e81e24e8
oiratka <nomadoirat88@gmail.com>
2024-11-10 20:37:50 -0500

    add folder components and move files

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import TodoList from "./TodoList";
-import AddToDoForm from "./AddToDoForm";
+
+import AddToDoForm from "./components/AddToDoForm";
+import TodoList from "./components/TodoList";
 
 const App = () => {
   const [newTodo, setNewTodo] = useState("");
