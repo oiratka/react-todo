4e642b3c866412be637e243c18be05b7801cc15c
oiratka <nomadoirat88@gmail.com>
2025-02-02 20:52:30 -0500

    add some CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-import AddToDoForm from "./components/AddToDoForm";
+import AddToDoForm from "./components/AddTodoForm/AddToDoForm";
 import TodoList from "./components/TodoList";
 
 const App = () => {
