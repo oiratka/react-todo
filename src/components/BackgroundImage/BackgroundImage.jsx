b05e5735e186169a57e37054b3e08fadfe59af1a
oiratka <nomadoirat88@gmail.com>
2025-02-27 00:37:51 -0500

    change photo home

--- src/components/BackgroundImage/BackgroundImage.jsx
+++ src/components/BackgroundImage/BackgroundImage.jsx
@@ -7,7 +7,7 @@ const BackgroundImage = () => {
     useEffect(() => {
         let backgroundImage = '';
         if (location.pathname === '/'){
-            backgroundImage = "url(https://images.unsplash.com/photo-1712000155290-ee65c0a82eda?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
+            backgroundImage = "url(https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
         } else if (location.pathname === '/new'){
             backgroundImage = "url(https://images.unsplash.com/photo-1528938102132-4a9276b8e320?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
         }
